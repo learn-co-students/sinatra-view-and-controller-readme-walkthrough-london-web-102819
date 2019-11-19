96962bdbddbd6c918af2252e1717f070092487ab
github username <harr.hughes@gmail.com>
2019-11-19 11:07:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,16 +1,29 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    @user = "Ian"
+
+    erb :index
+  end
+
+  get '/profile' do
+    erb :profile
+  end
+
   get '/reverse' do
     erb :reverse
   end
 
   post '/reverse' do
+    orig_string = params["string"]
+    @reversed_string = orig_string.reverse
     erb :reversed
   end
 
   get '/friends' do
-    # Write your code here!
-
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+    erb :friends
   end
 end
